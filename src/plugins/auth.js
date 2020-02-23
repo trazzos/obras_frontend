51f6c850782e5ec19dd2f1fe29416fa341a61783
mwleinad <dlopez@trazzos.com>
2020-02-22 18:08:04 -0600

    logout working

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -1,11 +1,11 @@
-import bearer from '@websanova/vue-auth/drivers/auth/bearer'
+import basic from '@websanova/vue-auth/drivers/auth/basic'
 import axios from '@websanova/vue-auth/drivers/http/axios.1.x'
 import router from '@websanova/vue-auth/drivers/router/vue-router.2.x'
 
 // Auth base configuration some of this options
 // can be override in method calls
 const config = {
-  auth: bearer,
+  auth: basic,
   http: axios,
   router: router,
   tokenDefaultName: 'laravel-vue-spa',
@@ -13,7 +13,7 @@ const config = {
   rolesVar: 'role',
   registerData: { url: 'auth/register', method: 'POST', redirect: '/login' },
   loginData: { url: 'auth/login', method: 'POST', redirect: '', fetchUser: true },
-  logoutData: { url: 'auth/logout', method: 'POST', redirect: '/', makeRequest: true },
+  logoutData: { url: 'auth/logout', method: 'POST', redirect: '/login', makeRequest: true },
   fetchData: { url: 'auth/user', method: 'GET', enabled: true },
   refreshData: { url: 'auth/refresh', method: 'GET', enabled: true, interval: 30 },
 }
